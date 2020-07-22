408a3d0618e584c7224bfa7bfcc7a8ec01f0843b
Kevin Talmadge <kevin@eluv.io>
2020-07-22 11:25:05 -0700

    Fix loading of overlay tags

--- src/stores/Overlay.js
+++ src/stores/Overlay.js
@@ -16,40 +16,50 @@ class OverlayStore {
 
   @action.bound
   AddOverlayTracks = flow(function * () {
-    const metadata = this.rootStore.videoStore.metadata;
-    if(!metadata.video_tags || !metadata.video_tags.overlay_tags) {
-      return;
-    }
+    try {
+      const metadata = this.rootStore.videoStore.metadata;
+      if(!metadata.video_tags || !metadata.video_tags.overlay_tags) {
+        return;
+      }
 
-    let overlayTagChunks = [];
-    for(let i = 0; i < Object.keys(metadata.video_tags.overlay_tags).length; i++) {
-      const {overlay_tags} = yield this.rootStore.client.LinkData({
-        versionHash: this.rootStore.videoStore.versionHash,
-        linkPath: `video_tags/overlay_tags/${i}`,
-        format: "json"
+      let overlayTagChunks = [];
+      const tagFiles = Object.keys(metadata.video_tags.overlay_tags);
+      for(let i = 0; i < tagFiles.length; i++) {
+        const {overlay_tags} = yield this.rootStore.client.LinkData({
+          versionHash: this.rootStore.videoStore.versionHash,
+          linkPath: `video_tags/overlay_tags/${tagFiles[i]}`,
+          format: "json"
+        });
+
+        overlayTagChunks.push(overlay_tags.frame_level_tags);
+      }
+
+      if(overlayTagChunks.length === 0) {
+        return;
+      }
+
+      const overlayTags = Object.assign({}, ...overlayTagChunks);
+
+      Object.keys(Object.values(overlayTags)[0]).forEach(trackKey => {
+        this.enabledOverlayTracks[trackKey] = true;
+
+        const metadataTrack = this.rootStore.trackStore.tracks.find(track => track.key === trackKey);
+
+        if(metadataTrack) {
+          metadataTrack.hasOverlay = true;
+          this.trackMap[trackKey] = metadataTrack.trackId;
+        }
       });
 
-      overlayTagChunks.push(overlay_tags.frame_level_tags);
+      // Overlay track is not observable for memory purposes
+      this.overlayTrack = overlayTags;
+      this.overlayEnabled = true;
+    } catch(error) {
+      // eslint-disable-next-line no-console
+      console.error("Failed to load overlay tracks:");
+      // eslint-disable-next-line no-console
+      console.error(error);
     }
-
-    if(overlayTagChunks.length === 0) { return; }
-
-    const overlayTags = Object.assign({}, ...overlayTagChunks);
-
-    Object.keys(Object.values(overlayTags)[0]).forEach(trackKey => {
-      this.enabledOverlayTracks[trackKey] = true;
-
-      const metadataTrack = this.rootStore.trackStore.tracks.find(track => track.key === trackKey);
-
-      if(metadataTrack) {
-        metadataTrack.hasOverlay = true;
-        this.trackMap[trackKey] = metadataTrack.trackId;
-      }
-    });
-
-    // Overlay track is not observable for memory purposes
-    this.overlayTrack = overlayTags;
-    this.overlayEnabled = true;
   });
 
   @action.bound
