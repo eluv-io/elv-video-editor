e522705569b7f5baad004ce5a649d22cca34e863
Kevin Talmadge <kevin@eluv.io>
2020-07-22 19:28:37 -0700

    Fix tag file loading

--- src/stores/Video.js
+++ src/stores/Video.js
@@ -4,7 +4,6 @@ import UrlJoin from "url-join";
 import URI from "urijs";
 import HLS from "hls.js";
 import {DownloadFromUrl} from "../utils/Utils";
-import LodashObject from "lodash/object";
 
 class VideoStore {
   @observable videoKey = 0;
@@ -238,22 +237,35 @@ class VideoStore {
           } else {
             let video_level_tags = {};
             let metadata_tags = {};
-            yield this.rootStore.client.utils.LimitedMap(
+
+            // Load and merge tag files
+            const tagData = yield this.rootStore.client.utils.LimitedMap(
               5,
               Object.keys(this.metadata.video_tags.metadata_tags),
-              async key => {
-                const tags = await this.rootStore.client.LinkData({
-                  versionHash: this.versionHash,
-                  linkPath: `video_tags/metadata_tags/${key}`,
-                  format: "json"
-                });
+              async fileLink => await this.rootStore.client.LinkData({
+                versionHash: this.versionHash,
+                linkPath: `video_tags/metadata_tags/${fileLink}`,
+                format: "json"
+              })
+            );
 
-                if(tags) {
-                  video_level_tags = LodashObject.merge(video_level_tags, tags.video_level_tags);
-                  metadata_tags = LodashObject.merge(metadata_tags, tags.metadata_tags);
+            tagData.forEach(tags => {
+              if(tags) {
+                video_level_tags = Object.assign(video_level_tags, tags.video_level_tags);
+
+                if(tags.metadata_tags) {
+                  Object.keys(tags.metadata_tags).forEach(trackKey => {
+                    if(metadata_tags[trackKey]) {
+                      metadata_tags[trackKey].tags = metadata_tags[trackKey].tags
+                        .concat(tags.metadata_tags[trackKey].tags)
+                        .sort((a, b) => a.startTime < b.startTime ? -1 : 1);
+                    } else {
+                      metadata_tags[trackKey] = tags.metadata_tags[trackKey];
+                    }
+                  });
                 }
               }
-            );
+            });
 
             this.tags = {
               video_level_tags,
